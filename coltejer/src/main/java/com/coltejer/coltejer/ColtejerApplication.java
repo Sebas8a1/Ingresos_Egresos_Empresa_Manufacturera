9f42f8b046a5553b4cc73aba38bd1d17a10a6ee8
FuryBaco <jarodriguez5699@misena.edu.co>
2022-08-25 01:29:19 -0500

    Añadiendo proyecto de empresa de manufactura2 q

---
+++ coltejer/src/main/java/com/coltejer/coltejer/ColtejerApplication.java
@@ -0,0 +1,13 @@
+package com.coltejer.coltejer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColtejerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColtejerApplication.class, args);
+	}
+
+}
